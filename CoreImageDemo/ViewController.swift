5a1167c09998a19efefa23665455275d59479071
rocooshiang <rocoo.shaing@gmail.com>
2016-03-25 17:35:26 +0800

    First commit

--- CoreImageDemo/ViewController.swift
+++ CoreImageDemo/ViewController.swift
@@ -9,17 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet var myImg: UIImageView?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    guard let image = myImg?.image, cgimg = image.CGImage else{
+      print("myImg doesn't have an image!")
+      return
+    }
+    
+    //Use CIContext to select GPU or CPU
+    let openGLContext = EAGLContext(API: .OpenGLES3)
+    let context = CIContext(EAGLContext: openGLContext)
+    
+    let coreImage = CIImage(CGImage: cgimg)
+    
+    let sepiaFilter = CIFilter(name: "CISepiaTone")
+    sepiaFilter?.setValue(coreImage, forKey: kCIInputImageKey)
+    sepiaFilter?.setValue(0.2, forKey: kCIInputIntensityKey)
+    
+    if let sepiaOutput = sepiaFilter?.valueForKey(kCIOutputImageKey) as? CIImage{
+      let exposureFilter = CIFilter(name: "CIExposureAdjust")
+      exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
+      exposureFilter?.setValue(1, forKey: kCIInputEVKey)
+      
+      if let exposureOutput = exposureFilter?.valueForKey(kCIOutputImageKey) as? CIImage{
+        let output = context.createCGImage(exposureOutput, fromRect: exposureOutput.extent)
+        let result = UIImage(CGImage: output)
+        myImg?.image = result
+      }
+      
+    }else{
+      print("image filtering failed")
+      
+    }
+    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
   }
-
-
+  
+  func coreImage(){
+    guard let image = myImg?.image, cgimg = image.CGImage else{
+      print("myImg doesn't have an image!")
+      return
+    }
+    
+    //Use CIContext to select GPU or CPU
+    let openGLContext = EAGLContext(API: .OpenGLES3)
+    let context = CIContext(EAGLContext: openGLContext)
+    
+    let coreImage = CIImage(CGImage: cgimg)
+    
+    let filter = CIFilter(name: "CISepiaTone")
+    filter?.setValue(coreImage, forKey: kCIInputImageKey)
+    filter?.setValue(0.2, forKey: kCIInputIntensityKey)
+    
+    if let output = filter?.valueForKey(kCIOutputImageKey) as? CIImage{
+      let cgimgresult = context.createCGImage(output, fromRect: output.extent)
+      let filteredImage = UIImage(CGImage: cgimgresult)
+      myImg?.image = filteredImage
+      
+    }else{
+      print("image filtering failed")
+      
+    }
+    
+    
+  }
+  
+  
 }
 
